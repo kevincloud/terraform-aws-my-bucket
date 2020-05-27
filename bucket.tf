addae3cc91e9a1cc77003915e39f93bd60e201a9
Kevin Cochran <kcochran@hashicorp.com>
2020-05-27 19:55:24 -0400

    initial commit

---
+++ bucket.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "module_bucket" {
+    bucket = "supercool-hashibucket-${var.identifier}"
+    acl = "public-read"
+    force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "module_bucket_policy" {
+    bucket = aws_s3_bucket.module_bucket.id
+
+    policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "ImageBucketPolicy",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.module_bucket.id}/*"
+    }
+  ]
+}
+POLICY
+}
